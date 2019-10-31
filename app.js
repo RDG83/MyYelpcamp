d567261e75c88e6c2594ac8fe8a76e474d51378f
Robert de Grijs <rdgrijs@hotmail.com>
2019-10-31 14:35:03 +0100

    add connect env

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const commentRoutes = require("./routes/comments");
 const campgroundRoutes = require("./routes/campgrounds");
 const indexRoutes = require("./routes/index");
 const flash = require("connect-flash");
-require("dotenv").config();
 
 // Connect to local DB
 // mongoose.connect("mongodb://localhost:27017/yelpcamp", {
@@ -24,7 +23,8 @@ require("dotenv").config();
 // Connect to Atlas DB
 mongoose.connect(process.env.DB_URL, {
   useCreateIndex: true,
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 app.set("view engine", "ejs");
